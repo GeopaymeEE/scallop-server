c60e724cfd226c714e9f8291aa7d549e79b9ee48
Kefkius <kefkius@mail.com>
2015-05-26 18:41:46 -0400

    irc channel

--- src/storage.py
+++ src/storage.py
@@ -66,7 +66,7 @@ class Storage(object):
         print_log("UTXO tree root hash:", self.root_hash.encode('hex'))
         print_log("Coins in database:", v)
 
-    # convert between bitcoin addresses and 20 bytes keys used for storage. 
+    # convert between clam addresses and 20 bytes keys used for storage. 
     def address_to_key(self, addr):
         return bc_address_to_hash_160(addr)
 
@@ -105,7 +105,7 @@ class Storage(object):
     def listunspent(self, addr):
         key = self.address_to_key(addr)
         if key is None:
-            raise BaseException('Invalid Bitcoin address', addr)
+            raise BaseException('Invalid CLAM address', addr)
 
         out = []
         for k, v in self.db_utxo.iterator(start=key):
